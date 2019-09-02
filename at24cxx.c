0a6732c008d25f77f58f0dde0d9fdb8e8f265038
Zhengyonghui <949546418@qq.com>
2019-09-02 22:04:30 +0800

    i2c driver 4th

--- at24cxx.c
+++ at24cxx.c
@@ -5,26 +5,34 @@
 #include <linux/jiffies.h>
 #include <linux/i2c.h>
 #include <linux/mutex.h>
+#include <linux/fs.h>
+
+/******************************************************************************/
+static int major;
+static struct class *cls;
 
 /******************************************************************************/
 static int at24cxx_detect(struct i2c_adapter *adapter, int address, int kind);
 static int at24cxx_attach(struct i2c_adapter *adapter);
 static int at24cxx_detach(struct i2c_client *client);
+static ssize_t at24cxx_read(struct file *file, char __user *buf, size_t size, loff_t *offset);
+static ssize_t at24cxx_write(struct file *file, const char __user *buf, size_t size, loff_t *offset);
+
 
 /*******************************************************************************/
 
 
 static unsigned short ignore[]  =   { I2C_CLIENT_END };
 static unsigned short normal_addr[] =   {0x50, I2C_CLIENT_END};
-static unsigned short forces_addr[] =   { ANY_I2C_BUS, 0x60, I2C_CLIENT_END };
-static unsigned short *forces[]     =   {  forces_addr, NULL};
+//static unsigned short forces_addr[] =   { ANY_I2C_BUS, 0x60, I2C_CLIENT_END };
+//static unsigned short *forces[]     =   {  forces_addr, NULL};
 
 /*******************************************************************************/
 static struct i2c_client_address_data addr_data = {
-    .normal_i2c = ignore,
+    .normal_i2c = normal_addr,
     .probe      = ignore,
     .ignore     = ignore,
-    .forces     = forces,
+ // .forces     = forces,
 };
 
 
@@ -35,11 +43,32 @@ static struct i2c_driver at24cxx_driver = {
     .attach_adapter = at24cxx_attach,
     .detach_client  = at24cxx_detach,
 };
+
+
+static struct file_operations at24cxx_fops = {
+    .owner  =   THIS_MODULE,
+    .read   =   at24cxx_read,
+    .write  =   at24cxx_write,
+};
 /******************************************************************************/
 
 static int at24cxx_detect(struct i2c_adapter *adapter, int address, int kind)
 {
+
+    struct i2c_client *new_client;
     printk("at24cxx_detect\r\n");
+    new_client  = kzalloc(sizeof(struct i2c_client),  GFP_KERNEL);
+    new_client->addr    =   address;
+    new_client->adapter =   adapter;
+    new_client->driver  =   &at24cxx_driver;
+    strcpy(new_client->name, "at24cxx");
+    i2c_attach_client(new_client);
+
+    
+    major   =   register_chrdev(0, "at24cxx", &at24cxx_fops);
+    cls     =   class_create(THIS_MODULE, "at24cxx");
+    class_device_create(cls, NULL, MKDEV(major, 0), NULL, "at24cxx");
+    
     return 0;
 }
 
@@ -54,6 +83,17 @@ static int at24cxx_detach(struct i2c_client *client)
     printk("at24cxx_detach!\r\n");
     return 0;
 }
+
+static ssize_t at24cxx_read(struct file *file, char __user *buf, size_t size, __kernel_loff_t *offset)
+{
+    return 0;
+}
+
+static ssize_t at24cxx_write(struct file *file, const char __user *buf, size_t size, __kernel_loff_t *offset)
+{
+    return 0;
+}
+
 
 
 
